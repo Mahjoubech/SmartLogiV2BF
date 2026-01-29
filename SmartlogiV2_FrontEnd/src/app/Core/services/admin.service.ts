5518c8ef461e21e5993f85859b567d75c59bf973
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-01-29 03:27:31 +0100

    Add admin service for user and manager management with CRUD operations

---
+++ SmartlogiV2_FrontEnd/src/app/Core/services/admin.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private apiUrl = `${environment.apiUrl}/admin`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(page: number = 0, size: number = 10): Observable<any> {
+    const params = new HttpParams().set('page', page).set('size', size);
+    return this.http.get<any>(`${this.apiUrl}/users/all`, { params });
+  }
+
+  blockUser(id: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/users/block/${id}`, {});
+  }
+
+  unblockUser(id: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/users/unblock/${id}`, {});
+  }
+
+  // Manager CRUD
+  getAllManagers(page: number = 0, size: number = 10): Observable<any> {
+    const params = new HttpParams().set('page', page).set('size', size);
+    return this.http.get<any>(`${this.apiUrl}/manager/all`, { params });
+  }
+
+  createManager(managerData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/manager/create`, managerData);
+  }
+
+  updateManager(id: string, managerData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/manager/update/${id}`, managerData);
+  }
+
+  deleteManager(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/manager/delete/${id}`);
+  }
+}
